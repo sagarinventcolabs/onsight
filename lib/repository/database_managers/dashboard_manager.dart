158f01781acca2a4045447a899d5b131cdb03fb0
Sagar Shrivastava <sagar.s@dreamorbit.com>
2023-04-19 15:04:02 +0530

    Merge code of sprint  7

--- lib/repository/database_managers/dashboard_manager.dart
+++ lib/repository/database_managers/dashboard_manager.dart
@@ -59,13 +59,7 @@ class DashboardManager{
   }
 
 
-  Future<dynamic> deleteAllData()async{
-    Database db = await DatabaseHelper().database;
-    String query="DELETE FROM $mDashboardTable";
-    var result = await db.rawQuery(query);
-    return result;
 
-  }
 
   Future<dynamic> getAllData()async{
     Database db = await DatabaseHelper().database;
